8a6ff7947e293bed2c760770f61d449e0069a49e
Ben <benlacroix83@gmail.com>
2016-10-20 21:37:59 +0800

    update code to swift 3 + autolayout

--- FacialRecognition/ViewController.swift
+++ FacialRecognition/ViewController.swift
@@ -10,71 +10,71 @@ import UIKit
 import CoreImage
 
 class ViewController: UIViewController {
-    @IBOutlet var imageView : UIImageView!
+    @IBOutlet var imageReceivedImageView : UIImageView!
                             
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let ciImage  = CIImage(CGImage:imageView.image!.CGImage!)
+        let ciImage  = CIImage(cgImage:imageReceivedImageView.image!.cgImage!)
         let ciDetector = CIDetector(ofType:CIDetectorTypeFace
-            ,context:nil
+            ,context:CIContext()
             ,options:[
                 CIDetectorAccuracy:CIDetectorAccuracyHigh,
                 CIDetectorSmile:true
             ]
         )
-        let features = ciDetector.featuresInImage(ciImage)
+        let features = ciDetector?.features(in: ciImage)
         
-        UIGraphicsBeginImageContext(imageView.image!.size)
-        imageView.image!.drawInRect(CGRectMake(0,0,imageView.image!.size.width,imageView.image!.size.height))
+        UIGraphicsBeginImageContext(imageReceivedImageView.image!.size)
+        imageReceivedImageView.image!.draw(in: CGRect(x:0,y:0,width:imageReceivedImageView.image!.size.width,height:imageReceivedImageView.image!.size.height))
         
-        for feature in features{
+        for feature in features!{
             
             //context
             let drawCtxt = UIGraphicsGetCurrentContext()
             
             //face
             var faceRect = (feature as! CIFaceFeature).bounds
-            faceRect.origin.y = imageView.image!.size.height - faceRect.origin.y - faceRect.size.height
-            CGContextSetStrokeColorWithColor(drawCtxt, UIColor.redColor().CGColor)
-            CGContextStrokeRect(drawCtxt,faceRect)
+            faceRect.origin.y = imageReceivedImageView.image!.size.height - faceRect.origin.y - faceRect.size.height
+            drawCtxt!.setStrokeColor(UIColor.red.cgColor)
+            drawCtxt!.stroke(faceRect)
             
             //mouse
             if (feature as! CIFaceFeature).hasMouthPosition != false{
-                let mouseRectY = imageView.image!.size.height - (feature as! CIFaceFeature).mouthPosition.y
-                let mouseRect  = CGRectMake((feature as! CIFaceFeature).mouthPosition.x - 5,mouseRectY - 5,10,10)
-                CGContextSetStrokeColorWithColor(drawCtxt,UIColor.blueColor().CGColor)
-                CGContextStrokeRect(drawCtxt,mouseRect)
+                let mouseRectY = imageReceivedImageView.image!.size.height - (feature as! CIFaceFeature).mouthPosition.y
+                let mouseRect  = CGRect(x:(feature as! CIFaceFeature).mouthPosition.x - 5,y:mouseRectY - 5,width:10,height:10)
+                drawCtxt!.setStrokeColor(UIColor.blue.cgColor)
+                drawCtxt!.stroke(mouseRect)
             }
-
+            
             //hige
             let higeImg      = UIImage(named:"hige_100.png")
-            let mouseRectY = imageView.image!.size.height - (feature as! CIFaceFeature).mouthPosition.y
+            let mouseRectY = imageReceivedImageView.image!.size.height - (feature as! CIFaceFeature).mouthPosition.y
             //ヒゲの横幅は顔の4/5程度
             let higeWidth  = faceRect.size.width * 4/5
             let higeHeight = higeWidth * 0.3 // 元画像が100:30なのでWidthの30%が縦幅
-            let higeRect  = CGRectMake((feature as! CIFaceFeature).mouthPosition.x - higeWidth/2,mouseRectY - higeHeight/2,higeWidth,higeHeight)
-            CGContextDrawImage(drawCtxt,higeRect,higeImg!.CGImage)
-
+            let higeRect  = CGRect(x:(feature as! CIFaceFeature).mouthPosition.x - higeWidth/2,y:mouseRectY - higeHeight/2,width:higeWidth,height:higeHeight)
+            drawCtxt?.draw(higeImg!.cgImage!, in: higeRect)
+            
             //leftEye
             if(feature as! CIFaceFeature).hasLeftEyePosition != false{
-                let leftEyeRectY = imageView.image!.size.height - (feature as! CIFaceFeature).leftEyePosition.y
-                let leftEyeRect  = CGRectMake((feature as! CIFaceFeature).leftEyePosition.x - 5,leftEyeRectY - 5,10,10)
-                CGContextSetStrokeColorWithColor(drawCtxt, UIColor.blueColor().CGColor)
-                CGContextStrokeRect(drawCtxt,leftEyeRect)
+                let leftEyeRectY = imageReceivedImageView.image!.size.height - (feature as! CIFaceFeature).leftEyePosition.y
+                let leftEyeRect  = CGRect(x:(feature as! CIFaceFeature).leftEyePosition.x - 5,y:leftEyeRectY - 5,width:10,height:10)
+                drawCtxt!.setStrokeColor(UIColor.blue.cgColor)
+                drawCtxt!.stroke(leftEyeRect)
             }
             
             //rightEye
             if (feature as! CIFaceFeature).hasRightEyePosition != false{
-                let rightEyeRectY = imageView.image!.size.height - (feature as! CIFaceFeature).rightEyePosition.y
-                let rightEyeRect  = CGRectMake((feature as! CIFaceFeature).rightEyePosition.x - 5,rightEyeRectY - 5,10,10)
-                CGContextSetStrokeColorWithColor(drawCtxt, UIColor.blueColor().CGColor)
-                CGContextStrokeRect(drawCtxt,rightEyeRect)
+                let rightEyeRectY = imageReceivedImageView.image!.size.height - (feature as! CIFaceFeature).rightEyePosition.y
+                let rightEyeRect  = CGRect(x:(feature as! CIFaceFeature).rightEyePosition.x - 5,y:rightEyeRectY - 5,width:10,height:10)
+                drawCtxt!.setStrokeColor(UIColor.blue.cgColor)
+                drawCtxt!.stroke(rightEyeRect)
             }
         }
         let drawedImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        imageView.image = drawedImage
+        imageReceivedImageView.image = drawedImage
     }
 
     override func didReceiveMemoryWarning() {
